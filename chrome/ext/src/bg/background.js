b28f2d41e358b2c101defa94c609108b70e311b8
avck <avck.tara@gmail.com>
2015-01-07 01:35:30 +0530

    Some Crud Sprinkled

--- chrome/ext/src/bg/background.js
+++ chrome/ext/src/bg/background.js
@@ -1,44 +1,11 @@
-//Checks the version of chrome and appropriate method for msg passing
-if (!chrome.runtime) {
-    // Chrome 20-21
-    chrome.runtime = chrome.extension;
-} else if(!chrome.runtime.onMessage) {
-    // Chrome 22-25
-    chrome.runtime.onMessage = chrome.extension.onMessage;
-    chrome.runtime.sendMessage = chrome.extension.sendMessage;
-    chrome.runtime.onConnect = chrome.extension.onConnect;
-    chrome.runtime.connect = chrome.extension.connect;
-}
+window.addEventListener("DOMContentLoaded", init, false);
 
-//Listener for connections from other tabs
-chrome.runtime.onConnect.addListener(function(port){
-	console.log("chrome.runtime.onConnect.addListener called for background page.\n Attempting to send a message now...");
-  	port.postMessage({greeting:"hello"});
-  	port.onMessage.addListener(
-  		function(message, sender) {
-  			console.log(message);
-  			scholar.indexedDB.addsnippets(message.text,message.URL);
-  		});
-});
-//Message Listener
-chrome.runtime.onMessage.addListener(
-	function(message,sender){
-			console.log("hello ::"+message);
-			scholar.indexedDB.addsnippets(message.text);
-			scholar.indexedDB.getAllsnippets();
-		});
+function init() {
+      scholar.indexedDB.open(); // open displays the data previously saved
+      // scholar.indexedDB.flushStores();
+  }
 
-window.onload = function initial () {
-	console.log( "Hi, background has been inited! Let the fun begin");
-	//paste the selected data to console.
-		//copy the selected dat 					window.getSelectedText().toString()
-		//store it to a variable					selected = window.getSelectedText().toString()
-		//pass the reference here					
-	chrome.tabs.create({url:"reviewsnips.html",active:true});
-
-	// console.log( copied);
-}
-
+  
 /*
 	Database Stuff
 	Using IndexedDB for storage!
@@ -49,7 +16,7 @@ window.onload = function initial () {
 	scholar.indexedDB.db = null;
 
 	scholar.indexedDB.open = function() {	
-  	var version = 1;
+  	var version = 3;
   		// snippets is the name of database
     var request = indexedDB.open("snippets", version);
   
@@ -74,27 +41,28 @@ window.onload = function initial () {
          db.deleteObjectStore("groups");
     }
 
-    if(db.objectStoreNames("books")){
+    if(db.objectStoreNames.contains("books")){
         db.deleteObjectStore("books");
     }
 
    	var snip_store = db.createObjectStore("snips",
-      		{keyPath: "timeStamp"});
-  	};
+      		{autoincrement: true});
   
- //    snip_store.createIndex("by_tags","tags");
+  
+    snip_store.createIndex("by_tags","tags");
 
- //    var group_store = db.createObjectStore("groups",
- //         {keyPath: "timeStamp"});
- //    };
- //    group_store.createIndex("by_tags","tags");
+    var group_store = db.createObjectStore("groups",
+         {autoincrement: true});
+  
+    group_store.createIndex("by_tags","tags");
 
- //    var book_store = db.createObjectStore("books",
- //        {keyPath: "timeStamp"});
- //    };
- //    book_store.createIndex("by_tags","tags");
+    var book_store = db.createObjectStore("books",
+        {autoincrement: true});
+    book_store.createIndex("by_tags","tags");
+    };
+  
 
-	// };
+	 };
 
 	//Adding objects
 	scholar.indexedDB.addsnippets = function(snip,url) {
@@ -123,6 +91,11 @@ window.onload = function initial () {
   		};
 	};
 
+  //Search by Tags 
+  //**TODO** 
+  // UNTESTED
+  //Handling the search results
+  //Create a Json and return to renderer
   scholar.indexedDB.searchByTags = function(objectStoreName, tag){
     
     var db = scholar.indexedDB.db;
@@ -143,6 +116,12 @@ window.onload = function initial () {
     };
 };
 
+  /*
+  update snippets by id
+  pass the id, which field to update new value and 
+  replace : boolean 1 for replacing the content, 0 for appending in say array fields
+
+  */
   scholar.indexedDB.updateSnippetsById = function (id, field, newValue, replace) {
     
     var db = scholar.indexedDB.db;
@@ -174,6 +153,7 @@ window.onload = function initial () {
       }
     };
   }
+
 	//Retrieving objects
 	scholar.indexedDB.getAllsnippets = function() {
   		
@@ -206,11 +186,11 @@ window.onload = function initial () {
   		cursorRequest.onerror = scholar.indexedDB.onerror;
 	};
 
-	//Deleting objects
-	scholar.indexedDB.deletesnips = function(id) {
+	//Deleting objects by ID from objectstore
+	scholar.indexedDB.deletesnips = function(objectStoreName, id) {
   		var db = scholar.indexedDB.db;
   		var trans = db.transaction(["snippets"], "readwrite");
-  		var store = trans.objectStore("snips");
+  		var store = trans.objectStore(objectStoreName);
 
   		var request = store.delete(id);
 
@@ -222,13 +202,61 @@ window.onload = function initial () {
     	console.log(e);
   		};
 	};
+ //flushing the stores
 
-	function init() {
-  		scholar.indexedDB.open(); // open displays the data previously saved
-	}
+ scholar.indexedDB.flushStores = function(){
+      var db = scholar.indexedDB.db;
+      var trans = db.transaction(["snippets"], "readwrite");
+      var store = trans.objectStore("snips");
+      store.clear();
 
-	window.addEventListener("DOMContentLoaded", init, false);
+      store = trans.objectStore("books");
+      store.clear();
+
+      store = trans.objectStore("groups");
+      store.clear();
+ }
 /*
 	********************
 */
 
+//Checks the version of chrome and appropriate method for msg passing
+if (!chrome.runtime) {
+    // Chrome 20-21
+    chrome.runtime = chrome.extension;
+} else if(!chrome.runtime.onMessage) {
+    // Chrome 22-25
+    chrome.runtime.onMessage = chrome.extension.onMessage;
+    chrome.runtime.sendMessage = chrome.extension.sendMessage;
+    chrome.runtime.onConnect = chrome.extension.onConnect;
+    chrome.runtime.connect = chrome.extension.connect;
+}
+
+//Listener for connections from other tabs
+chrome.runtime.onConnect.addListener(function(port){
+  console.log("chrome.runtime.onConnect.addListener called for background page.\n Attempting to send a message now...");
+    port.postMessage({greeting:"hello"});
+    port.onMessage.addListener(
+      function(message, sender) {
+        console.log(message);
+        scholar.indexedDB.addsnippets(message.text,message.URL);
+      });
+});
+//Message Listener
+chrome.runtime.onMessage.addListener(
+  function(message,sender){
+      console.log("hello ::"+message);
+      scholar.indexedDB.addsnippets(message.text);
+      scholar.indexedDB.getAllsnippets();
+    });
+
+window.onload = function initial () {
+  console.log( "Hi, background has been inited! Let the fun begin");
+  //paste the selected data to console.
+    //copy the selected dat           window.getSelectedText().toString()
+    //store it to a variable          selected = window.getSelectedText().toString()
+    //pass the reference here         
+  chrome.tabs.create({url:"reviewsnips.html",active:true});
+
+  // console.log( copied);
+}
